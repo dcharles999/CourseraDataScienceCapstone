36578a03a83c1c75daba8c2be721fc8a7b22175b
dcharles999 <diegocharles999@gmail.com>
2018-01-09 06:52:03 -0600

    Get data

---
+++ acquire.R
@@ -0,0 +1,5 @@
+
+if(!file.exists("Coursera-SwiftKey.zip")){
+   download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip","Coursera-SwiftKey.zip")
+   unzip("Coursera-SwiftKey.zip")
+}
